c2db273197c3370e30fddb899960b9a898454e75
Elena Pavlova <pavlova.elena.d4@gmail>
2026-09-16 00:11:17 +0300

    Fix circular import in customer.py

--- app/people/customer.py
+++ app/people/customer.py
@@ -1,7 +1,7 @@
-from app.people.customer import Customer
+class Customer:
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
 
-
-class CinemaBar:
-    @staticmethod
-    def sell_product(product: str, customer: Customer) -> None:
-        print(f"Cinema bar sold {product} to {customer.name}.")
+    def watch_movie(self, movie: str) -> None:
+        print(f'{self.name} is watching "{movie}".')
